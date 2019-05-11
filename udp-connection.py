5d5781d17a4f1ca4be2a37509c531a66b9979ce0
MaximevanderSmissen <m.vandersmissen@student.tudelft.nl>
2019-05-11 22:08:09 +0200

    Order thing that's not working

--- udp-connection.py
+++ udp-connection.py
@@ -1,5 +1,6 @@
 import socket
 import re
+from Communication import *
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
@@ -7,6 +8,7 @@ address=("188.166.115.7",7001)
 sock.bind(("",5000))
 sock.sendto("TYPE=SUBSCRIPTION_REQUEST".encode("ascii"),address)
 
+sender = Communication(("188.166.115.7",8001))
 
 while True:
     data,addr = sock.recvfrom(1024)
@@ -33,6 +35,25 @@ while True:
 
 
         print(bid_price)
+
+        if bid_price >= 3090:
+            sender.send(Order(feedcode, "SELL", bid_price, 1))
+            print("Order Send!")
+
+        elif ask_price <= 3070:
+            sender.send(Order(feedcode, "BUY", ask_price, 1))
+            print("Order Send!")
     
+    print("test1")
+    recieveData,a = sender.sock.recvfrom(1024)
+    print("test2")
+    recieveData = recieveData.decode('utf-8')
+    print("test3")
+
+    if recieveData.find("TYPE=ORDER_ACK")==-1:
+        print("Order Acknowledged!!!")
+        fields = recieveData.split('|')
+        print(fields[len(fields)-1])
+
 
     
